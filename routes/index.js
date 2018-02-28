f6461adcfc4bfdba7ed4ff1453d7cef6223fcaa8
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-28 11:30:15 +0700

    Register User Page

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.render('home/index');
 });
+router.get('/register', (req, res) => {
+  res.render('users/register');
+
+});
 module.exports = router;
