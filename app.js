0fc61dd1a31e418703222f30fe40534fb08a3da9
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-28 11:18:40 +0700

    Initial Commit - install express and make folder stucture

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+// const router = require('./routes/index');
+
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+app.set('views', __dirname+'/views/');
+app.set('view engine', 'ejs');
+
+//body parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+//routes
+// app.use('/', router);
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log(`Server Starts on ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Home');
+});
+module.exports = router;
