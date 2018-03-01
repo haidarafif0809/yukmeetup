8bb7e22aa6a00a600220b823db3d73f795c2ef43
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-01 13:52:38 +0700

    Join Event, Joined Event, validation : cannot join twice

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,7 +1,7 @@
 
 const isLogin = (req,res,next) => {
   if (req.session.user == null || req.session.user == undefined) {
-    res.redirect('/');
+    res.redirect('/users/login');
   } else {
     next();
   }
